0954c5bb98083380facebdb7db142b770d605508
jwilly117 <jakewilliams117@gmail.com>
2021-03-03 21:12:40 -0500

    fixing error

--- robots/robotsapp.js
+++ robots/robotsapp.js
@@ -1,4 +1,4 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/8.2.9/firebase.js"></script>
   
 
